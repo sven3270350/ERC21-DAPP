236f4d1716cdb6f925ba5943faa782b3d5548b1c
sven3270350 <sven3270350@gmail.com>
2024-05-31 19:21:47 +0530

    Add: Transaction Cost Estimation Component

--- src/components/executeProject/deploy-token.tsx
+++ src/components/executeProject/deploy-token.tsx
@@ -11,6 +11,7 @@ interface DeployTokenProps {
 export const DeployToken = ({ projectId }: DeployTokenProps) => {
   const [isDeploying, setIsDeploying] = useState(false);
   const [deployState, setDeployState] = useState(false);
+
   const handleDeployment = async () => {
     try {
       setIsDeploying(true);
@@ -32,17 +33,14 @@ export const DeployToken = ({ projectId }: DeployTokenProps) => {
         tokenData.tokenName,
         tokenData.tokenSymbol,
         tokenData.maxSupply,
-        tokenData.initialSupply,
-        {
-          gasLimit: 1500000, // or any other value you deem appropriate
-        }
+        tokenData.initialSupply
       );
+
       await contract.deployed();
 
       console.log("Contract deployed at address:", contract.address);
       tokenData.contractAddress = contract.address;
       parsedData[projectId].tokenDetails = tokenData;
-      console.log(parsedData);
       localStorage.setItem("projectData", JSON.stringify(parsedData));
 
       setIsDeploying(false);
@@ -66,4 +64,32 @@ export const DeployToken = ({ projectId }: DeployTokenProps) => {
       )}
     </div>
   );
+};
+
+export const getTxCost = async ({ projectId }: DeployTokenProps) => {
+  const rpc = process.env.NEXT_PUBLIC_RPC!;
+  const provider = new ethers.providers.JsonRpcProvider(rpc);
+
+  const privateKey = process.env.NEXT_PUBLIC_WALLET_PRIVATE_KEY!;
+  const wallet = new ethers.Wallet(privateKey, provider);
+
+  const factory = new ethers.ContractFactory(abi, bytecode, wallet);
+  const existingData = localStorage.getItem("projectData");
+
+  const parsedData: Record<string, any> = existingData
+    ? JSON.parse(existingData)
+    : {};
+  const tokenData = parsedData[projectId].tokenDetails;
+
+  const deployTransaction = await factory.getDeployTransaction(
+    tokenData.tokenName,
+    tokenData.tokenSymbol,
+    tokenData.maxSupply,
+    tokenData.initialSupply
+  );
+  const estimateGasLimit = await wallet.estimateGas(deployTransaction);
+  const gasPrice = await provider.getGasPrice();
+  const estimatedTransactionCost = estimateGasLimit.mul(gasPrice);
+
+  return ethers.utils.formatEther(estimatedTransactionCost);
 };
