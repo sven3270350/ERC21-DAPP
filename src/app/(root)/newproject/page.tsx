807575c5d1273a0be587ae696f159cf6e57f66db
sven3270350 <sven3270350@gmail.com>
2024-06-04 18:57:44 +0530

    Transfer page Ui and sidebar header fix

--- src/app/(root)/newproject/page.tsx
+++ src/app/(root)/newproject/page.tsx
@@ -5,10 +5,8 @@ import { ScrollArea } from "@/components/ui/scroll-area";
 export default function newprojecthome() {
 
     return (
-        // <ScrollArea className="w-full h-full overflow-x-auto overflow-y-auto ">
             <div className="w-full h-full bg-[rgb(9,9,11)] overflow-y-auto ">
                 <MultipleSteppes />
             </div>
-        // </ScrollArea>
     );
 }
