7354ca17da1a79229534469b20b6b568522335c0
sven3270350 <sven3270350@gmail.com>
2024-06-04 20:24:27 +0530

    Add: Create Pool Functionality

--- src/components/executeProject/CreatePool.tsx
+++ src/components/executeProject/CreatePool.tsx
@@ -1,9 +1,53 @@
 import Image from "next/image";
 import React from "react";
+import { ethers } from "ethers";
+import { writeContract, prepareWriteContract, readContract } from "@wagmi/core";
+import { parseEther, parseUnits } from "viem";
+import { abi } from "@/constants/routerABI.json";
+import { useAccount } from "wagmi";
 interface CreatePoolProps {
   onPrev?: () => void;
+  projectId: string;
 }
-const CreatePool: React.FC<CreatePoolProps> = ({ onPrev }) => {
+const CreatePool: React.FC<CreatePoolProps> = ({ onPrev, projectId }) => {
+  const { address } = useAccount();
+
+  const handlePoolCreation = async () => {
+    const existingData = localStorage.getItem("projectData");
+    const parsedData: Record<string, any> = existingData
+      ? JSON.parse(existingData)
+      : {};
+    const tokenData = parsedData[projectId].tokenDetails;
+    const poolData = parsedData[projectId].poolData;
+    const tokenAddress = tokenData?.contractAddress;
+    const amountTokenDesired = poolData.tokenLiquidityAmount;
+    const amountETHDesired = poolData.ethLiquidityAmount;
+    const deadline = Math.floor(Date.now() / 1000) + 60 * 20; // 20 minutes from now
+
+    try {
+      const { request } = await prepareWriteContract({
+        abi,
+        address: "0xC532a74256D3Db42D0Bf7a0400fEFDbad7694008",
+        functionName: "addLiquidityETH",
+        args: [
+          tokenAddress,
+          amountTokenDesired,
+          amountTokenDesired,
+          amountETHDesired,
+          address,
+          deadline,
+        ],
+        value: parseUnits(amountETHDesired, 18),
+      });
+      console.log(request);
+      const hash = await writeContract(request);
+
+      console.log(hash);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <div className="p-6 flex flex-col items-center gap-6 border border-[#18181B] rounded-xl ">
       <div className="flex flex-col items-center gap-2">
@@ -29,7 +73,10 @@ const CreatePool: React.FC<CreatePoolProps> = ({ onPrev }) => {
             />{" "}
             Back
           </button>
-          <button className="bg-[#F57C00] text-black px-8 py-2 text-sm font-bold  rounded-md ">
+          <button
+            onClick={handlePoolCreation}
+            className="bg-[#F57C00] text-black px-8 py-2 text-sm font-bold  rounded-md "
+          >
             Execute
           </button>
         </div>
