112773bacd60d8096b707b55405fda39deed461c
sven3270350 <sven3270350@gmail.com>
2024-06-08 00:18:21 +0800

    removing drizzle - fix build

--- src/db/schema/users.ts
+++
@@ -1,8 +0,0 @@
-import { pgTable, serial, varchar } from "drizzle-orm/pg-core";
- 
-export const users = pgTable('users', {
-  id: serial('id').primaryKey(),
-  email: varchar('email'),
-  name: varchar('name'),
-  address: varchar('address')
-});
