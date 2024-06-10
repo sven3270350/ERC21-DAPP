4da020fff7413a30c637ca519ccebbb062e95b76
sven3270350 <sven3270350@gmail.com>
2024-06-10 11:41:27 -0500

    remove comment

--- src/app/api/polling/log/[userId]/[projectId]/route.ts
+++ src/app/api/polling/log/[userId]/[projectId]/route.ts
@@ -9,9 +9,9 @@ export async function GET(
 ) {
   const session = await getServerSession(authOptions)
 
-  // if (!session) {
-  //   return NextResponse.json({ message: "Unauthorized" }, { status: 401 })
-  // }
+  if (!session) {
+    return NextResponse.json({ message: "Unauthorized" }, { status: 401 })
+  }
 
   try {
     const userRequests = await prisma.transactionRequest.findMany({
