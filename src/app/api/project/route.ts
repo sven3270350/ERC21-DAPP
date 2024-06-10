156cf92c362fe7af556faf71b1380daaf2087424
sven3270350 <sven3270350@gmail.com>
2024-06-10 10:57:11 -0500

    add project PUT api

--- src/app/api/project/route.ts
+++ src/app/api/project/route.ts
@@ -58,3 +58,80 @@ export async function POST(request: NextRequest) {
     return NextResponse.json({ success: false, error: error })
   }
 }
+
+export async function PUT(request: NextRequest) {
+  const session = await getServerSession(authOptions)
+
+  if (!session) {
+    return NextResponse.json({ message: "Unauthorized" }, { status: 401 })
+  }
+
+  const data = await request.json()
+
+  const projectData = data.projectData;
+  const userId = Number(data.userId);
+  const projectId = data.projectId;
+  const projectIds = Object.keys(projectData)
+
+  if (!projectId) {
+    return NextResponse.json({ message: "ProjectId is required." }, { status: 401 })
+  }
+
+  if (!projectData) {
+    return NextResponse.json({ message: "ProjectData is required." }, { status: 401 })
+  }
+
+  if (!userId) {
+    return NextResponse.json({ message: "userId is required." }, { status: 401 })
+  }
+
+
+  try {
+    for(let i = 0; i < projectIds.length; i++) {
+      const project = projectData[projectIds[i]];
+      const projectResult = await prisma.project.findFirst({
+        where: {
+          projectId: projectIds[i]
+        }
+      })
+
+      if (!projectResult) {
+        return NextResponse.json({ message: "Invalid projectId." }, { status: 401 })
+      }
+
+      if (!project.beneficiaryDetails || !project.beneficiaryDetails.wallets) {
+        await prisma.project.updateMany({
+          where: {
+            projectId: projectId,
+            userId: userId
+          },
+          data: {
+            projectId: projectIds[i],
+            userId: userId,
+            projectData: JSON.stringify(project)
+          }
+        })
+      } else {
+        const encryptedWallets = project.beneficiaryDetails.wallets.map((value: any) => {
+          return {...value, privateKey: encrypt(value.privateKey)}
+        })
+        project.beneficiaryDetails.wallets = encryptedWallets;
+        await prisma.project.updateMany({
+          where: {
+            projectId: projectId,
+            userId: userId
+          },
+          data: {
+            projectId: projectIds[i],
+            userId: userId,
+            projectData: JSON.stringify(project)
+          }
+        })
+      }
+    }
+  
+    return NextResponse.json({ success: true, message: "Projects successfully updated." })
+  } catch (error) {
+    return NextResponse.json({ success: false, error: error })
+  }
+}
