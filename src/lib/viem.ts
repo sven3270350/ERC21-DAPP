8130daaacac9f7462aecb4e68565b140ecec41ed
sven3270350 <sven3270350@gmail.com>
2024-06-13 23:29:53 +0800

    fix viem default RPC

--- src/lib/viem.ts
+++ src/lib/viem.ts
@@ -1,8 +1,8 @@
 import { createPublicClient, http } from 'viem'
 import { mainnet, sepolia } from 'viem/chains'
 
-const chain = process.env.NEXT_PUBLIC_ENVIRONMENT === "development" ? sepolia : mainnet;
-const transport = process.env.NEXT_PUBLIC_ENVIRONMENT === "development" ? http("https://sepolia.drpc.org") : http()
+const chain = process.env.NEXT_PUBLIC_ENVIRONMENT == "development" ? sepolia : mainnet;
+const transport = http(process.env.NEXT_PUBLIC_ALCHEMY_RPC);
  
 export const publicClient = createPublicClient({
   chain: chain,
